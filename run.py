d6365a078a7228ed554406c2e76167850302f48c
Matt <mjd014@live.co.uk>
2022-08-30 18:36:46 +0000

    created contact.hmtl, added route to all html files in run.py.

--- run.py
+++ run.py
@@ -4,10 +4,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Using @app.route("/"), this opens our index.html page.
+# The route is a "/" because in our url there is nothing at the end of it.
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# This connects to our about.html page.
+# The route is "/about" because in our url at the end of it is "/about."
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
