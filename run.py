d7e85204968b024e50fb6824c866c4711e96c5cd
Matt <mjd014@live.co.uk>
2022-09-03 14:05:52 +0000

    created a new argument within the /about route, list_of_numbers, has a list as its value.

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    return render_template("about.html", page_title="About", list_of_numbers=[1,2,3])
 
 
 # This connects to our contact.html page.
